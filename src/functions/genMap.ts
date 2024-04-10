3c929f956332268b1ed8b63e9f4e3ebc763792ed
꧁Ashû꧂ <30575805+Ashu11-A@users.noreply.github.com>
2024-04-10 10:01:34 -0400

    🤣 If an egg can't be processed, just skip it

--- src/functions/genMap.ts
+++ src/functions/genMap.ts
@@ -8,44 +8,49 @@ export async function genMap(eggs: string[], branch: string, repository: string)
     const EggsMapping: EggMap[] = []
 
     for (const egg of eggs) {
-        const file = readFileSync(egg, 'utf8')
-        const { size } = statSync(egg)
-        const { name, description, author, exported_at } = JSON.parse(file) as EggConfig
-        const pathEgg = egg.replace(`${egg.split('/')[0]}/`, '')
-        const link = `https://raw.githubusercontent.com/${repository}/${branch}/${pathEgg}`
-        const language = await cld.detect(description)
-            .then(({ languages }) => {
-                return languages.reduce((max, language) => language.percent >  max.percent ?? 0 ? language : max).code
-            })
-            .catch((err: Error) => {
-                console.log({
-                    error: err.message,
-                    egg: egg.split('/').pop(),
-                    link,
-                    description,
+        try {
+            const file = readFileSync(egg, 'utf8')
+            const { size } = statSync(egg)
+            const { name, description, author, exported_at } = JSON.parse(file) as EggConfig
+            const pathEgg = egg.replace(`${egg.split('/')[0]}/`, '')
+            const link = `https://raw.githubusercontent.com/${repository}/${branch}/${pathEgg}`
+            const language = await cld.detect(description)
+                .then(({ languages }) => {
+                    return languages.reduce((max, language) => language.percent >  max.percent ?? 0 ? language : max).code
                 })
-                return 'en'
+                .catch((err: Error) => {
+                    console.log({
+                        error: err.message,
+                        egg: egg.split('/').pop(),
+                        link,
+                        description,
+                    })
+                    return 'en'
+                })
+
+            let type = undefined
+            const languages = ['pt-br', 'en']
+            const pathEggSplit = pathEgg.split('/')
+            if (pathEggSplit[0].toLowerCase() !== 'eggs') {
+                type = pathEggSplit[0].toLowerCase().replace('game_eggs', 'games')
+            } else if (languages.some(language => pathEggSplit[1].toLowerCase() === language)) {
+                type = pathEggSplit[2]
+            }
+
+            EggsMapping.push({
+                author,
+                name,
+                description,
+                exported_at,
+                link,
+                size: formatBytes(size),
+                language,
+                type: type ?? null
             })
-
-        let type = undefined
-        const languages = ['pt-br', 'en']
-        const pathEggSplit = pathEgg.split('/')
-        if (pathEggSplit[0].toLowerCase() !== 'eggs') {
-            type = pathEggSplit[0].toLowerCase().replace('game_eggs', 'games')
-        } else if (languages.some(language => pathEggSplit[1].toLowerCase() === language)) {
-            type = pathEggSplit[2]
+        } catch {
+            console.log(`Ocorreu um erro ao tentar processar o arquivo: ${egg}`)
+            continue
         }
-
-        EggsMapping.push({
-            author,
-            name,
-            description,
-            exported_at,
-            link,
-            size: formatBytes(size),
-            language,
-            type: type ?? null
-        })
     }
 
     writeFile(`api/${repository.split('/')[0]}.min.json`, JSON.stringify(EggsMapping), {}, ((err) => {
